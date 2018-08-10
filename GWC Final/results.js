23eb5b0e7b2a453c733f31d8f54d104489fb8743
karla-ravin <41647831+karla-ravin@users.noreply.github.com>
2018-08-10 09:52:31 -0400

    Add files via upload

---
+++ GWC Final/results.js
@@ -0,0 +1,365 @@
+anDanbury = {
+  "Name" : "Tails of Courage",
+  "Description" : "Dog walkers, foster homes, volunteers at shelters, volunteers for adoption events, spreading the word, clean up, transportation",
+  "Address" : "39 Smith St, Danbury CT, 06810",
+  "Link" : "https://tailsofcourage.org/fostervolunteering/"
+};
+
+var animals = {
+    "Bridgeport" : "No repsonses matching search",
+    "Danbury" : anDanbury,
+    "Glastonbury" : "No repsonses matching search",
+    "Greenwich" : "No repsonses matching search",
+    "Hartford" : "No repsonses matching search",
+    "Litchfield" : "No repsonses matching search",
+    "Middletown" : "No repsonses matching search",
+    "Haven" : "No repsonses matching search",
+    "London" : "No repsonses matching search",
+    "Norwalk" : "No repsonses matching search",
+    "Stamford" : "No repsonses matching search",
+    "Waterbury" : "No repsonses matching search"
+};
+/////////////////////////////////////////////////////////////////////////////////////
+var pplBridgeport = {
+  "Name" : "Daniel trust foundation",
+  "Description" : "Invests in and supports low income students’ educational and professional goals",
+  "Address" : "285 Fairfield Avenue, Bridgeport CT, 06604",
+  "Link" : "https://www.danieltrustfoundation.org/"
+};
+
+var pplDanbury = {
+  "Name" : "The Salvation Army Family Store and Donation Center",
+  "Description" : "Help disaster survivors, cure hunger, overcome poverty, serve the LGBTQ community, provide shelter, fight human trafficking, stop domestic abuse, equip families, teach kids, empower the arts, assist the unemployed, combat addiction, love the elderly, serve veterans, share god's love, and meet the greatest need",
+  "Address" : "129 Main St, Danbury CT, 06810",
+  "Link" : "https://www.salvationarmyusa.org/usn/"
+};
+var pplGlastonbury = {
+  "Name" : "Youth and Family Services",
+  "Description" : "Glastonbury youth and family services is a town agency devoted to programs and services that respond to the needs of glastonbury youth and their family",
+  "Address" : "",
+  "Link" : "http://www.glastonbury-ct.gov/departments/department-directory-l-z/youth-and-family-services#ad-image-2"
+};
+
+var pplHartford = {
+  "Name" : "Connecticut Children’s Medical Center",
+  "Description" : "Connecticut Children’s Medical Center is dedicated to improving the physical and emotional health of children through family-centered care, research, education and advocacy. They embrace discovery, teamwork, integrity and excellence in all that we do. They are making children in Connecticut the healthiest in the country",
+  "Address" : "282 Washington St, Hartford, CT 06106",
+  "Link" : "https://www.connecticutchildrens.org/about-us/mission/"
+};
+
+var pplHaven = {
+  "Name" : "Cents of Relief",
+  "Description" : "This non-profit help people in vulnerable situations especially women in prostitution. They provide healthcare for their kids. They also help in the effort of stopping human trafficking",
+  "Address" : "109 Church Street, Apt 202 New Haven CT 06510 USA",
+  "Link" : "https://greatnonprofits.org/org/centsofrelief"
+};
+var pplLondon = {
+  "Name" : "Birthright- Israel",
+  "Description" : "Birthright guiding principle It is the right of every pregnant woman to give birth and the right of every child to be born. Birthright offers the alternative to abortion. Birthright is pro-life. Birthright is non-profit, interdenominational, and non-political. Help make a difference in a mother's life as she prepares for the birth of her baby",
+  "Address" : "26 Phillips St, New London, CT 06901",
+  "Link" : "https://www.birthrightisrael.com/"
+};
+var pplNorwalk = {
+  "Name" : "Person 2 Person",
+  "Description" : "Person 2 Person is an organization that helps to less fortunate. They have events such as food pantries,P2P on wheels, and food drive",
+  "Address" : "76 S Main St, Norwalk, CT 06854",
+  "Link" : "https://p2phelps.org/volunteers/#volunteer-programs"
+};
+var pplStamford = {
+  "Name" : "SunRise Senior Living",
+  "Description" : "A nursing home for the elderly and also a its a rehab center for the injured",
+  "Address" : "251 Turn of River Road  Stamford  CT  06905",
+  "Link" : "https://www.sunriseseniorliving.com/"
+};
+
+var people = {
+    "Bridgeport" : pplBridgeport,
+    "Danbury" : pplDanbury,
+    "Glastonbury" : pplGlastonbury,
+    "Greenwich" : "No repsonses matching search",
+    "Hartford" : pplHartford,
+    "Litchfield" : "No repsonses matching search",
+    "Middletown" : "No repsonses matching search",
+    "Haven" : pplHaven,
+    "London" : pplLondon,
+    "Norwalk" : pplNorwalk,
+    "Stamford" : pplStamford,
+    "Waterbury" : "No repsonses matching search"
+};
+////////////////////////////////////////////////////////////
+
+var oneDanbury = {
+  "Name" : "Tails of Courage",
+  "Description" : "Dog walkers, foster homes, volunteers at shelters, volunteers for adoption events, spreading the word, clean up, transportation",
+  "Address" : "39 Smith St, Danbury CT, 06810",
+  "Link" : "https://tailsofcourage.org/fostervolunteering/"
+};
+
+var oneGlastonbury = {
+  "Name" : "Glastonbury Food Bank",
+  "Description" : "Provide educational, creative, and social benefits for all children with particular reference to children with special needs",
+  "Address" : "2155 Main St, Glastonbury CT",
+  "Link" : "http://www.childrensworldcharity.org/"
+};
+
+var oneday = {
+    "Bridgeport" : "No repsonses matching search",
+    "Danbury" : oneDanbury,
+    "Glastonbury" : oneGlastonbury,
+    "Greenwich" : "No repsonses matching search",
+    "Hartford" : "No repsonses matching search",
+    "Litchfield" : "No repsonses matching search",
+    "Middletown" : "No repsonses matching search",
+    "Haven" : "No repsonses matching search",
+    "London" : "No repsonses matching search",
+    "Norwalk" : "No repsonses matching search",
+    "Stamford" : "No repsonses matching search",
+    "Waterbury" : "No repsonses matching search"
+};
+
+/////////////////////////////////////////////////////////////
+
+var campHartford = {
+  "Name" : "Girl Scouts",
+  "Description" : "Provides girls with leadership skills and the confidence to take challenges head on and to let them know that woman have a place in this world also",
+  "Address" : "340 Washington St, Hartford, CT 06106",
+  "Link" : "https://www.girlscouts.org/en/adults/volunteer.html "
+};
+
+var campHaven = {
+  "Name" : "Solar Youth",
+  "Description" : "Solar Youth provides opportunities for young people to develop a positive sense of self and connection and commitment to others through programs that incorporate environmental exploration, leadership and community service",
+  "Address" : "53 Wayfarer St New Haven CT 06515 USA",
+  "Link" : "https://greatnonprofits.org/org/solar-youth-inc"
+};
+
+var campLondon = {
+  "Name" : "Girl Scouts",
+  "Description" : "Provides girls with leadership skills and the confidence to take challenges head on and to let them know that woman have a place in this world also",
+  "Address" : "21 Montauk Ave Ste 203 New London, CT 06320",
+  "Link" : "https://www.girlscouts.org/en/adults/volunteer.html "
+};
+
+var campNorwalk = {
+  "Name" : "George Washington Carver Community Center",
+  "Description" : "Community Center for children and Adults. They hold after school activities to help children improve their learning",
+  "Address" : "7 Academy Street Norwalk, CT 06850",
+  "Link" : "https://www.carvernorwalk.org/"
+};
+
+var campcoun = {
+    "Bridgeport" : "No repsonses matching search",
+    "Danbury" : "No repsonses matching search",
+    "Glastonbury" : "No repsonses matching search",
+    "Greenwich" : "No repsonses matching search",
+    "Hartford" : campHartford,
+    "Litchfield" : "No repsonses matching search",
+    "Middletown" : "No repsonses matching search",
+    "Haven" : campHaven,
+    "London" : campLondon,
+    "Norwalk" : campNorwalk,
+    "Stamford" : "No repsonses matching search",
+    "Waterbury" : "No repsonses matching search"
+};
+/////////////////////////////////////////////////////////////////////
+
+var comBridgeport = {
+  "Name" : "Service for peace inc.",
+  "Description" : "Volunteers participate in ongoing community development programs throughout the world",
+  "Address" : "360 Fairfield Ave Ste 206, Bridgeport CT, 06604",
+  "Link" : "http://www.serviceforpeace.org/"
+};
+
+var comDanbury = {
+  "Name" : "Housatonic Habitat for Humanity",
+  "Description" : "Provide affordable homes built by volunteers with materials giften by local businesses",
+  "Address" : "51 Austin St, Danbury CT, 06810",
+  "Link" : "http://housatonichabitat.org/about/what-we-do/"
+};
+
+var comGlastonbury= {
+  "Name" : "YMCA",
+  "Address" : "95 Oakwood Drive, Glastonbury CT, 06033",
+  "Description" : "You can help by choosing from the options of being a coach, volunteering, and doing community service",
+  "Link" : "http://www.ghymca.org/index.cfm"
+};
+
+var comGreenwich = {
+  "Name" : "American red cross",
+  "Description" : "Blood donations",
+  "Address" : "99 Indian Field Rd, Greenwich",
+  "Link" : "http://www.redcross.org/"
+}
+
+var comHartford = {
+  "Name" : "Stitch a Memory Bear",
+  "Description" : "Volunteer to sew a memory bear for someone who has lost a relative",
+  "Address" : "Locations throughout CT",
+  "Link" : "https://www.vitas.com/about-us/hospice-volunteers/hearts-and-hands-help-vitas-hospice-families"
+};
+
+var comHaven = {
+  "Name" : "Community Soup Kitchen",
+  "Description" : "Services meals all day and supplies food to the homeless",
+  "Address" : "84 Broadway New Haven CT 06511",
+  "Link" : "http://www.csknewhaven.org/"
+};
+
+var comLondon = {
+  "Name" : "Homeless Hospitality Center",
+  "Description" : "Volunteer opportunities that take as little as a few hours per month. You can help with a one-time project or come every week. They serve food, clean parks, etc.",
+  "Address" : "325 Huntington St New London, CT 06320",
+  "Link" : "https://www.nlhhc.org/volunteer"
+};
+
+var comNorwalk = {
+  "Name" : "Human Services Council",
+  "Description" : "They deal with all services that pertain to humans. Things such as food, clothing school supplies and they also deal with violence issues",
+  "Address" : "1 Park St, 2nd Floor, Norwalk, CT 06851",
+  "Link" : "https://hscct.org/"
+};
+
+var comStamford = {
+  "Name" : "The Jewish Community Center",
+  "Description" : "The Stamford JCC is a cultural, educational and recreational center, offering exceptional programs, classes and special events designed to enrich the mind, body and spirit. We are driven by the Jewish values of welcoming guests, kindness, respect, and communal responsibility, and we warmly welcome people of all ages, faiths, ethnicities and backgrounds",
+  "Address" : "1035 Newfield Avenue  Stamford, CT 06905",
+  "Link" : "http://www.stamfordjcc.org/"
+};
+
+var communityserv = {
+    "Bridgeport" : comBridgeport,
+    "Danbury" : comDanbury,
+    "Glastonbury" : comGlastonbury,
+    "Greenwich" : comGreenwich,
+    "Hartford" : comHartford,
+    "Litchfield" : "No repsonses matching search",
+    "Middletown" : "No repsonses matching search",
+    "Haven" : comHaven,
+    "London" : comLondon,
+    "Norwalk" : comNorwalk,
+    "Stamford" : comStamford,
+    "Waterbury" : "No repsonses matching search"
+};
+/////////////////////////////////////////////////////////////////
+var teachBridgeport = {
+  "Name" : "Mercy learning center",
+  "Description" : "Provides basic learning and literacy to low income women",
+  "Address" : "637 Park Ave, Bridgeport CT, 06604 ",
+  "Link" : "http://www.mercylearningcenter.org/"
+};
+
+var teachDanbury = {
+  "Name" : "The Salvation Army Family Store and Donation Center",
+  "Description" : "Help disaster survivors, cure hunger, overcome poverty, serve the LGBTQ community, provide shelter, fight human trafficking, stop domestic abuse, equip families, teach kids, empower the arts, assist the unemployed, combat addiction, love the elderly, serve veterans, share god's love, and meet the greatest need",
+  "Address" : "129 Main St, Danbury CT, 06810",
+  "Link" : "https://www.salvationarmyusa.org/usn/"
+};
+
+var teachGlastonbury = {
+  "Name" : "Children’s Advancement Fund",
+  "Description" : "A nonprofit organization, supports children with special needs and their families in connecticut by facilitating services such as occupational, physical, and speech/language therapy",
+  "Address" : "1062 Glastonbury CT 06033 USA ",
+  "Link" : "https://www.facebook.com/childrensadvancementfund/"
+};
+
+var teachHartford = {
+  "Name" : "The Salvation Army Family Store and Donation Center",
+  "Description" : "Help disaster survivors, cure hunger, overcome poverty, serve the LGBTQ community, provide shelter, fight human trafficking, stop domestic abuse, equip families, teach kids, empower the arts, assist the unemployed, combat addiction, love the elderly, serve veterans, share god's love, and meet the greatest need",
+  "Address" : "121/123 Sigourney Street Hartford,CT",
+  "Link" : "https://www.salvationarmyusa.org/usn/"
+};
+
+var teachHaven = {
+  "Name" : "All Our Kin",
+  "Description" : "It supports kids and gives them a fair shot at life. Makes that each kid learns the value of life and the value of education",
+  "Address" : "414 A Chapel St, New Haven, CT 06511 ",
+  "Link" : "https://greatnonprofits.org/org/all-our-kin-inc"
+};
+
+var teaching = {
+    "Bridgeport" : teachBridgeport,
+    "Danbury" : teachDanbury,
+    "Glastonbury" : teachGlastonbury,
+    "Greenwich" : "No repsonses matching search",
+    "Hartford" : teachHartford,
+    "Litchfield" : "No repsonses matching search",
+    "Middletown" : "No repsonses matching search",
+    "Haven" : teachHaven,
+    "London" : "No repsonses matching search",
+    "Norwalk" : "No repsonses matching search",
+    "Stamford" : "No repsonses matching search",
+    "Waterbury" : "No repsonses matching search"
+};
+
+///////////////////////////////////////////////////////////////////////
+
+var locationKeys = ["Name","Address","Description","Link"];
+
+//creates result string
+function returnLocationDict(dict){
+  var str = "";
+  for (var key in dict){
+    str += "<br>" + "<strong>" + key + ": </strong>";
+    if (key == "Link") {
+      str += "<a target='_blank' href=" + dict[key] + ">" + dict[key] + "</a></br>"
+    } else  {
+      str += dict[key] + "<br>"
+    }
+    console.log(str)
+  }
+  //fill the string
+  return str;
+}
+
+function returnServiceDict(dict, town){
+  var str;
+  var type = typeof dict[town];
+  if (type == "string"){
+    str = dict[town];
+  } else if (type == "object") {
+    str = returnLocationDict(dict[town]);
+  }
+    // str = str + dict[keys[i]];
+  return str;
+}
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+//////////////////////////////////////////////////////////////
+function showResults() {
+  var loc = getQueryVariable("location");
+  var serv = getQueryVariable("services")
+  var dict;
+
+  if (serv == "Animals") {
+    dict = animals;
+  };
+  if (serv == "People") {
+    dict = people;
+  };
+  if (serv == "Oneday") {
+    dict = oneday;
+  }
+  if (serv == "Campcoun") {
+    dict = campcoun;
+  }
+  if (serv == "Communityserv") {
+    dict = communityserv;
+  }
+  if (serv == "Teaching") {
+    dict = teaching;
+  }
+
+  console.log(dict)
+  var text = returnServiceDict(dict,loc);
+  document.getElementById("resultText").innerHTML=text;
+};
+
+showResults();
