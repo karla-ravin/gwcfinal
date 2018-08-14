0ea2a1b8871ce7e514fe9c69b33d8b20aa56c9fa
unknown <2karlaravin@gmail.com>
2018-08-14 09:33:34 -0400

    clean-up

--- GWC Final/volunteer.js
+++
@@ -1,7 +0,0 @@
-function run() {
-  var loc = document.getElementById("location").value;
-  var serv = document.getElementById("services").value;
-  var link = document.getElementById("results");
-  var path = "C:/Users/SYFGWC29/Desktop/karla-ravin.github.io/GWC%20Final/"
-  link.href = path+"results.html"+"?"+"location="+loc+"&services="+serv;
-}
