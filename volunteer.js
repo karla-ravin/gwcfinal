77568d186721ebf59de27f9a16f775696e8398bc
karla-ravin <41647831+karla-ravin@users.noreply.github.com>
2018-08-14 15:26:15 -0400

    Update volunteer.js

--- volunteer.js
+++ volunteer.js
@@ -2,6 +2,6 @@ function run() {
   var loc = document.getElementById("location").value;
   var serv = document.getElementById("services").value;
   var link = document.getElementById("results");
-  var path = "C:/Users/SYFGWC29/Desktop/karla-ravin.github.io/GWC%20Final/"
+  var path = "https://karla-ravin.github.io/gwcfinal/"
   link.href = path+"results.html"+"?"+"location="+loc+"&services="+serv;
 }
